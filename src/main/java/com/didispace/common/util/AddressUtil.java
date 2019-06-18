94df1aec2ea91ea3d700e679b66fbf507e21a464
23108 <2310874577@qq.com>
2019-06-18 13:43:02 +0800

    添加ip解析

--- src/main/java/com/didispace/common/util/AddressUtil.java
+++ src/main/java/com/didispace/common/util/AddressUtil.java
@@ -13,16 +13,11 @@ public class AddressUtil {
     private static final Logger logger = LoggerFactory.getLogger(AddressUtil.class);
 
     /**
-     *
-     *
      * String str = "{\"age\":\"24\",\"name\":\"cool_summer_moon\"}";
      * JSONObject jsonObject = JSONObject.parseObject(str);
      * System.out.println("json对象是：" + jsonObject);
      * Object object = jsonObject.get("name");
      * System.out.println("name值是："+object)
-     *
-     *
-     *
      */
 
 
@@ -34,29 +29,22 @@ public class AddressUtil {
                 String url = "http://ip.taobao.com/service/getIpInfo.php";
 
                 String rs = null;
-
-                while (true){
-                    try {
-                        rs = HttpUtils.sendPost(url, "ip=" + ip);
-                        if (!StringUtils.isEmpty(rs)){
-                            break;
-                        }
-                    } catch (IOException e) {
-                        logger.warn(e.getMessage());
-                    }
+                try {
+                    rs = HttpUtils.sendPost(url, "ip=" + ip);
+                } catch (IOException e) {
+                    logger.warn(e.getMessage());
                 }
-
-
-                JSONObject jsonObject = JSONObject.parseObject(rs);
-                JSONObject dataObject = JSONObject.parseObject(String.valueOf(jsonObject.get("data")));;
-                if (dataObject!=null){
-                    if (dataObject.get("region")==null){
+                if (!StringUtils.isEmpty(rs)) {
+                    JSONObject jsonObject = JSONObject.parseObject(rs);
+                    JSONObject dataObject = JSONObject.parseObject(String.valueOf(jsonObject.get("data")));
+                    ;
+                    if (dataObject.get("region") == null) {
                         return String.valueOf(dataObject.get("country"));
 
-                    }else {
-                        return String.valueOf(dataObject.get("region"))+ String.valueOf(dataObject.get("city"));
+                    } else {
+                        return String.valueOf(dataObject.get("region")) + String.valueOf(dataObject.get("city"));
                     }
-                }else{
+                } else {
                     return "";
                 }
             }
